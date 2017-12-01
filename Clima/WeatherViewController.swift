1db25133dba4e8aa5b1c897c3817d39a4c153dd1
Francis Jemuel Bergonia <francisb.itcareers@gmail.com>
2017-12-02 01:01:30 +0800

    For Update

--- Clima/WeatherViewController.swift
+++ Clima/WeatherViewController.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 import CoreLocation
+import Alamofire
+import SwiftyJSON
 
 class WeatherViewController: UIViewController , CLLocationManagerDelegate {
     
@@ -46,7 +48,9 @@ class WeatherViewController: UIViewController , CLLocationManagerDelegate {
     /***************************************************************/
     
     //Write the getWeatherData method here:
-    
+    func getWeatherData(url: String, parameter: [String: String]) {
+        
+    }
 
     
     
@@ -79,11 +83,28 @@ class WeatherViewController: UIViewController , CLLocationManagerDelegate {
     
     
     //Write the didUpdateLocations method here:
-    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations[locations.count - 1]
+        if location.horizontalAccuracy > 0 {
+            locationManager.stopUpdatingLocation()
+            
+            print("longitude = \(location.coordinate.longitude), latitude = \(location.coordinate.latitude)")
+            
+            let latitude = String(location.coordinate.latitude)
+            let longitude = String(location.coordinate.longitude)
+            
+            let params : [String : String] = ["late" : latitude, "lon" : longitude, "appid" : APP_ID]
+        
+            getWeatherData(url: WEATHER_URL, parameter: params)
+        }
+    }
     
     
     //Write the didFailWithError method here:
-    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
+        cityLabel.text = "Location Unavailable"
+    }
     
     
 
